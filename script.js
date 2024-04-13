31911533b09a76c3f26b69ebc95c460374882fb2
DiogenesHSCosta <diogenes.henriquecosta@outlook.com>
2024-04-12 22:44:18 -0300

    finalização código

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 let result = document.getElementById('resultado')
 
-function pegarDados(){
-    num1 = parseInt(document.getElementById("numero1").value)
-    num2 = parseInt(document.getElementById("numero2").value)
+function getDate(){
+    num1 = parseFloat(document.getElementById("numero1").value)
+    num2 = parseFloat(document.getElementById("numero2").value)
 }
 
 document.getElementById("btnSoma").addEventListener("click", () =>{
-    pegarDados()
+    getDate()
     let valor = num1 + num2;
     result.innerText = valor
 })
 
 document.getElementById("btnSubtracao").addEventListener("click", () => {
-    pegarDados()
+    getDate()
     let valor = num1 - num2;
     result.innerHTML = valor
 })
 
 document.getElementById("btnMultiplicacao").addEventListener("click", () =>{
-    pegarDados()
+    getDate()
     let valor = num1 * num2;
     result.innerHTML = valor
 })
 
 document.getElementById("btnDivisao").addEventListener("click", () =>{
-    pegarDados()
+    getDate()
     let valor = num1 / num2;
     result.innerHTML = valor
 })
